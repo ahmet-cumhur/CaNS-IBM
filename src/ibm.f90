47efd902832e7be6697a5648fd306b88edd370db
ahmet-cumhur <thecronlis@gmail.com>
2026-07-07 00:16:59 +0200

    thakkar(#5)

--- src/ibm.f90
+++ src/ibm.f90
@@ -7,7 +7,8 @@ module mod_ibm
     ! we initalize the ibm coef. here
     ! at the main.f90
     ! check if the real location(x,y,z) is in the given body shape 
-    logical function isInbody(ibm_direction,amp_l,n_wave,l_0,phase_l,x,y,z,n,l)
+    logical function isInbody(ibm_direction,amp_l,n_wave,l_0,phase_l,x,y,z,n,l,&
+                                hmap,l1_hmap,l2_hmap,n1_hmap,n2_hmap)
         implicit none
         logical , intent(in), dimension(0:1,3)   :: ibm_direction
         real(rp), intent(in), dimension(0:1,3)   :: amp_l
@@ -22,16 +23,43 @@ module mod_ibm
         integer                                  :: side,t
         real(rp)                                 :: xyz(3)
         integer                                  :: i,ii
+        real(rp),intent(in),optional             :: hmap(0:,0:)
+        real(rp),intent(in),optional             :: l1_hmap,l2_hmap 
+        integer,intent(in),optional              :: n1_hmap,n2_hmap
+        real(rp)                                 :: dl_hmap(2)
+        integer                                  :: i1(2),i2(2)
+        real(rp)                                 :: r1(2),r2(2)
+        real(rp)                                 :: w1(2),w2(2)
         xyz = [x,y,z]
         do side = 0,1
             do t = 1,3
                 i=modulo(t,3)+1
                 ii=modulo(t+1,3)+1
                 if(ibm_direction(side,t))then
-                    height(side,t)=amp_l(side,i)*0.5_rp*(1._rp+sin(2._rp*pi*&
-                                    real(n_wave(side,i)*xyz(i)/l(i),rp)+phase_l(side,i)))+&
-                                    amp_l(side,ii)*0.5_rp*(1._rp+sin(2._rp*pi*&
-                                    real(n_wave(side,ii)*xyz(ii)/l(ii),rp)+phase_l(side,ii)))
+                    ! use wave wall  
+                    if(.not. use_hmap)then
+                        height(side,t)=amp_l(side,i)*0.5_rp*(1._rp+sin(2._rp*pi*&
+                                        real(n_wave(side,i)*xyz(i)/l(i),rp)+phase_l(side,i)))+&
+                                        amp_l(side,ii)*0.5_rp*(1._rp+sin(2._rp*pi*&
+                                        real(n_wave(side,ii)*xyz(ii)/l(ii),rp)+phase_l(side,ii)))
+                    ! use hmap                
+                    else
+                         if(trim(hmap_mode)=="fit")then
+                            call get_hmap_loc(n1_hmap,n2_hmap,xyz(i),xyz(ii),l(i),l(ii),dl_hmap,i1,i2,r1,r2,w1,w2)
+                            ! Bilinear interpolation 
+                            height(side,t)=(hmap(i1(1),i2(2))*(w1(1))+hmap(i1(2),i2(2))*(w1(2)))*(w2(1))+&
+                                           (hmap(i1(1),i2(1))*(w1(1))+hmap(i1(2),i2(1))*(w1(2)))*(w2(2))
+                        elseif(trim(hmap_mode)=="normal")then
+                            ! here we need to change the shape of the hmap otherwise we will use different sized dl_hmap...
+                            call get_hmap_loc(n1_hmap,n2_hmap,xyz(i),xyz(ii),l1_hmap,l2_hmap,dl_hmap,i1,i2,r1,r2,w1,w2)
+                            ! Bilinear interpolation 
+                            height(side,t)=(hmap(i1(1),i2(2))*(w1(1))+hmap(i1(2),i2(2))*(w1(2)))*(w2(1))+&
+                                           (hmap(i1(1),i2(1))*(w1(1))+hmap(i1(2),i2(1))*(w1(2)))*(w2(2))
+                        else
+                            print*,"unknown hmap mode entered..."
+                            stop
+                        endif               
+                    endif
                 else
                     height(side,t)=0._rp
                 endif
@@ -53,34 +81,6 @@ module mod_ibm
             endif   
         end do 
     end function isInbody
-    subroutine interpolate_hmap(nx_hmap,ny_hmap,x,y,z,lx,ly,lz,dx_hmap,dy_hmap,&
-                                ivelx_l,ivelx_r,ively_t,ively_b,rvelx_l,rvelx_r,rvely_t,rvely_b)
-        ! first lets conver the dimensions of the hmap to the real locations.
-        real(rp),intent(in)     :: lx,ly,lz,x,y,z
-        integer,intent(in)      :: nx_hmap,ny_hmap
-        integer,intent(out)     :: ivelx_l,ivelx_r,ively_t,ively_b
-        real(rp),intent(out)    :: rvelx_l,rvelx_r,rvely_t,rvely_b
-        real(rp),intent(out)    :: dx_hmap,dy_hmap
-        ivelx_l=0;ivelx_r=0;ively_t=0;ively_b=0;
-        rvelx_l=0._rp;rvelx_r=0._rp;rvely_t=0._rp;rvely_b=0._rp;
-        dx_hmap=0._rp;dy_hmap=0._rp
-        dx_hmap=lx/nx_hmap
-        dy_hmap=ly/ny_hmap
-
-        ivelx_l= floor(real(x/dx_hmap,kind=rp))
-        ivelx_r=ivelx_l+1
-        rvelx_l=ivelx_l*dx_hmap
-        rvelx_r=ivelx_r*dx_hmap
-        ivelx_l = modulo(ivelx_l,nx_hmap)
-        ivelx_r = modulo(ivelx_r,nx_hmap)
-
-        ively_b=floor(real(y/dy_hmap,kind=rp))
-        ively_t=ively_b+1
-        rvely_t=ively_t*dy_hmap
-        rvely_b=ively_b*dy_hmap
-        ively_b = modulo(ively_b,ny_hmap)
-        ively_t = modulo(ively_t,ny_hmap)        
-    end subroutine interpolate_hmap
     subroutine get_hmap_loc(n1_hmap,n2_hmap,loc_1,loc_2,&
                             l1_hmap,l2_hmap,dl_hmap,i1,i2,r1,r2,w1,w2)
         integer,intent(in)                              :: n1_hmap,n2_hmap
@@ -121,71 +121,6 @@ module mod_ibm
         w2(2)=abs(loc_2-r2(1))/dl_hmap(2)
     end subroutine get_hmap_loc
 
-    logical function isInBody_hmap(ibm_direction,amp_l,n_wave,l_0,phase_l,x,y,z,n,l,&
-                                hmap,l1_hmap,l2_hmap,n1_hmap,n2_hmap)
-        implicit none
-        logical , intent(in), dimension(0:1,3)   :: ibm_direction
-        real(rp), intent(in), dimension(0:1,3)   :: amp_l
-        integer , intent(in), dimension(0:1,3)   :: n_wave
-        real(rp), intent(in), dimension(0:1,3)   :: l_0
-        real(rp), intent(in), dimension(0:1,3)   :: phase_l
-        integer , intent(in), dimension(3)       :: n
-        real(rp), intent(in), dimension(3)       :: l
-        real(rp),intent(in)                      :: x,y,z 
-        real(rp),parameter                       :: pi = 3.141592653589793_rp
-        real(rp)                                 :: height(0:1,3)
-        integer                                  :: side,t
-        real(rp)                                 :: xyz(3)
-        integer                                  :: i,ii
-        real(rp)                                 :: hmap(0:,0:)
-        real(rp),intent(in),optional             :: l1_hmap,l2_hmap 
-        integer                                  :: n1_hmap,n2_hmap
-        real(rp)                                 :: dl_hmap(2)
-        integer                                  :: i1(2),i2(2)
-        real(rp)                                 :: r1(2),r2(2)
-        real(rp)                                 :: w1(2),w2(2)
-        xyz = [x,y,z]
-        do side = 0,1
-            do t = 1,3
-                i=modulo(t,3)+1
-                ii=modulo(t+1,3)+1
-                if(ibm_direction(side,t))then
-                    if(trim(hmap_mode)=="fit")then
-                        call get_hmap_loc(n1_hmap,n2_hmap,xyz(i),xyz(ii),l(i),l(ii),dl_hmap,i1,i2,r1,r2,w1,w2)
-                        ! Bilinear interpolation 
-                        height(side,t)=(hmap(i1(1),i2(2))*(w1(1))+hmap(i1(2),i2(2))*(w1(2)))*(w2(1))+&
-                                       (hmap(i1(1),i2(1))*(w1(1))+hmap(i1(2),i2(1))*(w1(2)))*(w2(2))
-                    elseif(trim(hmap_mode)=="normal")then
-                        ! here we need to change the shape of the hmap otherwise we will use different sized dl_hmap...
-                        call get_hmap_loc(n1_hmap,n2_hmap,xyz(i),xyz(ii),l1_hmap,l2_hmap,dl_hmap,i1,i2,r1,r2,w1,w2)
-                        ! Bilinear interpolation 
-                        height(side,t)=(hmap(i1(1),i2(2))*(w1(1))+hmap(i1(2),i2(2))*(w1(2)))*(w2(1))+&
-                                       (hmap(i1(1),i2(1))*(w1(1))+hmap(i1(2),i2(1))*(w1(2)))*(w2(2))
-                    else
-                        print*,"unknown hmap mode entered..."
-                        stop
-                    endif
-                else
-                    height(side,t)=0._rp
-                endif
-            end do 
-        end do 
-        isInBody_hmap=.false.
-        do t = 1,3
-            if(ibm_direction(0,t))then
-                height(0,t)=l_0(0,t)+height(0,t)
-                if(xyz(t)<=height(0,t))then
-                    isInBody_hmap=.true.
-                endif
-            endif
-            if(ibm_direction(1,t))then
-                height(1,t)=l_0(1,t)-height(1,t)
-                if(xyz(t)>=height(1,t))then
-                    isInBody_hmap=.true.
-                endif
-            endif   
-        end do 
-    end function isInBody_hmap
     ! 1st order IBM 
     ! we change the diL depending the velocity mask we are handling 
     ! e.g. we need to apply 1,0,0 for mask_u and we need to apply 0,1,0 for mask_v 
@@ -222,16 +157,10 @@ module mod_ibm
                     x = (real(ii,rp) -0.5d0+ real(dix,rp)*0.5d0)*dl(1)
                     y = (real(jj,rp) -0.5d0+ real(diy,rp)*0.5d0)*dl(2)
                     z = (real(kk,rp) -0.5d0+ real(diz,rp)*0.5d0)*dl(3)
-                    if(.not.use_hmap)then
-                        if(isInbody(ibm_direction,amp_l,n_wave,l_0,phase_l,x,y,z,n,l).eqv..true.)then
-                            mask_id(i,j,k) = .true.
-                        endif
-                    else
-                        if(isInBody_hmap(ibm_direction,amp_l,n_wave,l_0,phase_l,x,y,z,n,l,&
+                    if(isInbody(ibm_direction,amp_l,n_wave,l_0,phase_l,x,y,z,n,l,&
                                 hmap,l1_hmap,l2_hmap,n1_hmap,n2_hmap).eqv..true.)then
-                            mask_id(i,j,k) = .true.
-                        endif
-                    endif
+                        mask_id(i,j,k) = .true.
+                    endif                    
                 end do 
             end do 
         end do
